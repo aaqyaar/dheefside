1e5b196dfd7f06e90abdaa530c3aae90305b4273
abdizamedmo <abdizamedmo@gmail.com>
2022-11-29 07:35:41 +0300

    back-end starting point is ready

---
+++ back-end/src/server.ts
@@ -0,0 +1,10 @@
+import app from "./main";
+import http from "http";
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`🌍 => Server is running on port ${PORT} <= 🌍`);
+});
