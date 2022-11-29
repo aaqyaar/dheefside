63ff3d9deaeae160c572ba4fe9e90446619cad68
abdizamedmo <abdizamedmo@gmail.com>
2022-11-29 15:09:23 +0300

    added tailwind css and apollo client + react helmet async

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  HttpLink,
+} from "@apollo/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: process.env.REACT_APP_GRAPHQL_API,
+  }),
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <Router>
+        <App />
+      </Router>
+    </ApolloProvider>
   </React.StrictMode>
-)
+);
--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import fs from "fs";
+
+const absolutePathAliases: { [key: string]: string } = {};
+const rootPath = path.resolve(__dirname, "src");
+
+const rootContents = fs.readdirSync(rootPath).map((file) => {
+  const filePath = path.resolve(rootPath, file);
+  const stat = fs.statSync(filePath);
+  return {
+    name: file,
+    path: filePath,
+    isDirectory: stat.isDirectory(),
+  };
+});
+
+const pages = rootContents.filter((file) => file.isDirectory);
+
+pages.forEach((page) => {
+  absolutePathAliases[page.name] = page.path;
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  build: {
+    outDir: "build",
+  },
+  resolve: {
+    alias: {
+      ...absolutePathAliases,
+    },
+  },
+  plugins: [react()],
+});
